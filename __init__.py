254c03c6f65c22fa44e205e3ffcb336c4db373b5
houdinii <houdinii@email.net>
2024-03-07 18:24:12 -0700

    update __init__.py to reflect new nodes

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 from .CropByAspectRatio import CropByAspectRatio
+from .ComfyImageToWand import ComfyImageToWand
+from .WandToComfyImage import WandToComfyImage
 
 NODE_CLASS_MAPPINGS = {
     "CropByAspectRatio": CropByAspectRatio,
+    "ComfyImageToWand": ComfyImageToWand,
+    "WandToComfyImage": WandToComfyImage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CropByAspectRatio": "Crop By Aspect Ratio",
+    "ComfyImageToWand": "Comfy Image To Wand Image",
+    "WandToComfyImage": "Wand Image To Comfy Image",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]

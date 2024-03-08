a5919961904f24b635cc7f6d204ae060ac8fa1c4
houdinii <houdinii@email.net>
2024-03-08 12:20:55 -0700

    add Colorize.py node

--- __init__.py
+++ __init__.py
@@ -2,12 +2,14 @@ from .src.transform.CropByAspectRatio import CropByAspectRatio
 from .src.sfx.BlueShift import BlueShift
 from .src.sfx.Charcoal import Charcoal
 from .src.sfx.AddNoise import AddNoise
+from .src.sfx.Colorize import Colorize
 
 NODE_CLASS_MAPPINGS = {
     "CropByAspectRatio": CropByAspectRatio,
     "BlueShift": BlueShift,
     "Charcoal": Charcoal,
     "AddNoise": AddNoise,
+    "Colorize": Colorize,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -15,6 +17,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "BlueShift": BlueShift.TITLE,
     "Charcoal": Charcoal.TITLE,
     "AddNoise": AddNoise.TITLE,
+    "Colorize": Colorize.TITLE
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]

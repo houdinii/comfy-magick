deca85e035f6474de318d7e6efe1ed9c36e598a5
houdinii <houdinii@email.net>
2024-03-07 18:37:01 -0700

    update CropByAspectRatio.py to use new pipeline

--- CropByAspectRatio.py
+++ CropByAspectRatio.py
@@ -1,44 +1,5 @@
-import PIL
-import torch
-import PIL.Image
-import numpy as np
-import tempfile
-
 import wand.image
-from wand.image import Image
-
-from .utilities import wand_to_pil, calculate_aspect_ratio
-
-
-def crop_by_aspect_ratio(image, ratio: str, gravity: str = None):
-    batch_size, old_height, old_width, channels = image.shape
-    new_width, new_height = calculate_aspect_ratio(ratio, old_width, old_height)
-
-    result = torch.zeros(
-        batch_size,
-        new_height,
-        new_width,
-        channels,
-        dtype=image.dtype,
-        device=image.device,
-    )
-
-    for b in range(batch_size):
-        img_b = image[b] * 255.0
-        img_b = PIL.Image.fromarray(
-            img_b.numpy().astype("uint8"), "RGB"
-        )
-        with tempfile.NamedTemporaryFile(suffix=".png", dir=".", delete=False) as tmp:
-            img_b.save(tmp.name, format="png")
-            with Image(filename=tmp.name) as wand_img:
-                if gravity in wand.image.GRAVITY_TYPES:
-                    wand_img.crop(width=new_width, height=new_height, gravity=gravity)
-                else:
-                    wand_img.crop(width=new_width, height=new_height)
-                result_b = wand_to_pil(wand_img)
-        result_b = torch.tensor(np.array(result_b)) / 255.0
-        result[b] = result_b
-    return result
+from .utilities import calculate_aspect_ratio
 
 
 class CropByAspectRatio:
@@ -46,7 +7,7 @@ class CropByAspectRatio:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "image": ("IMAGE",),
+                "WAND_IMAGES": ("WAND_IMAGES",),
                 "aspect_ratio": (
                     [
                         "1:1",
@@ -77,13 +38,20 @@ class CropByAspectRatio:
         }
 
     INPUT_IS_LIST = False
-    RETURN_TYPES = ("IMAGE",)
+    RETURN_TYPES = ("WAND_IMAGES",)
     FUNCTION = "processCropByAspectRatio"
     OUTPUT_NODE = False
     OUTPUT_IS_LIST = (False,)
 
     CATEGORY = "ComfyMagick"
 
-    def processCropByAspectRatio(self, image, aspect_ratio, gravity):
-        result = crop_by_aspect_ratio(image, aspect_ratio, gravity=gravity)
-        return (result,)
+    def processCropByAspectRatio(self, WAND_IMAGES, aspect_ratio, gravity):
+        old_width, old_height = WAND_IMAGES[0].size
+        new_width, new_height = calculate_aspect_ratio(aspect_ratio, old_width, old_height)
+
+        for i in range(len(WAND_IMAGES)):
+            if gravity in wand.image.GRAVITY_TYPES:
+                WAND_IMAGES[i].crop(width=new_width, height=new_height, gravity=gravity)
+            else:
+                WAND_IMAGES[i].crop(width=new_width, height=new_height)
+        return (WAND_IMAGES,)

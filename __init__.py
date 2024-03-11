c2116cdebea300f4a84f9a78864d993d8f49132d
houdinii <houdinii@email.net>
2024-03-10 20:07:32 -0600

    remove TestNative.py test file

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 from .src.transform.CropByAspectRatio import CropByAspectRatio
 
-from .src.sfx.TestNative import TestNative
-
 from .src.sfx.BlueShift import BlueShift
 from .src.sfx.Charcoal import Charcoal
 from .src.sfx.AddNoise import AddNoise
@@ -35,7 +33,6 @@ from .src.image_effects.UnsharpMask import UnsharpMask
 from .src.image_effects.Spread import Spread
 
 NODE_CLASS_MAPPINGS = {
-    "TestNative": TestNative,
     "CropByAspectRatio": CropByAspectRatio,
     "BlueShift": BlueShift,
     "Charcoal": Charcoal,
@@ -69,7 +66,6 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "TestNative": TestNative.TITLE,
     "CropByAspectRatio": CropByAspectRatio.TITLE,
     "BlueShift": BlueShift.TITLE,
     "Charcoal": Charcoal.TITLE,
